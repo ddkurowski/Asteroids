01843ff1d3508ed16dfaa17ecdf50b61bbd64ecd
ddkurowski <ddkurowski@gmail.com>
2025-11-08 14:14:29 -0600

    Completed draw and and logging

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pygame
 from constants import *
+from player import Player
+from logger import log_state
+
+# class Player:
+#     def __init__(self, x, y):
+#         self.x = SCREEN_WIDTH / 2
+#         self.y = SCREEN_HEIGHT / 2
+
+
 
 def main():
     print("Starting Asteroids!")
@@ -7,15 +16,24 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
    
     while True:
+        log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
                 
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
+        
+        
 
 
 
