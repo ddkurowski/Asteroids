8c5f2ecb0d330de46ce6728a97a96387ec9e70d0
ddkurowski <ddkurowski@gmail.com>
2025-11-10 16:21:52 -0600

    switched to groups for main game loop

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     clock = pygame.time.Clock()
     dt = 0
    
@@ -29,8 +34,9 @@ def main():
                 return
                 
         screen.fill(black)
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for items in drawable:
+            items.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60)) / 1000
         
